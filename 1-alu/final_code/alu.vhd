6de30d921ccbd8fcbab8cee5c5a07bb0656d1d5a
Ahmed Aladdin <aamieeu@gmail.com>
2024-10-11 21:50:16 +0300

    feature: build the full alu

--- 1-alu/final_code/alu.vhd
+++ 1-alu/final_code/alu.vhd
@@ -14,11 +14,12 @@ end entity alu;
 
 architecture alu_arch of alu is
     signal f0, f1, f2, f3: std_logic_vector(n-1 downto 0);
-    signal cout0, cout1, cout2, cout3: std_logic;
+    signal cout0, cout1, cout2, cout3, cout4: std_logic_vector(0 downto 0);
     component alu_a is
         generic (n: integer := 8);
         port (
             a, b: in std_logic_vector(n-1 downto 0);
+            cin: in std_logic;
             sel: in std_logic_vector(1 downto 0);
             f: out std_logic_vector(n-1 downto 0);
             cout: out std_logic
@@ -56,19 +57,26 @@ architecture alu_arch of alu is
     component mux4 is
         generic (n: integer := 8);
         port (
-            a,b: in std_logic_vector(n-1 downto 0);
+            a,b,c,d: in std_logic_vector(n-1 downto 0);
             sel: in std_logic_vector(1 downto 0);
-            f: out std_logic_vector(n-1 downto 0);
+            f: out std_logic_vector(n-1 downto 0)
         );
     end component;
     begin
-        alu_a: alu_a generic map (n) port map (a, b, sel(1 downto 0), f0, cout0);
-        alu_b: alu_b generic map (n) port map (a, b, sel(1 downto 0), f1, cout1);
-        alu_c: alu_c generic map (n) port map (a, sel(1 downto 0), cin, f2, cout2);
-        alu_d: alu_d generic map (n) port map (a, cin, sel(1 downto 0), f3, cout3);
+        alu_a_intance: alu_a generic map (n) port map (a, b, cin, sel(1 downto 0), f0, cout0(0));
+        alu_b_intance: alu_b generic map (n) port map (a, b, sel(1 downto 0), f1, cout1(0));
+        alu_c_intance: alu_c generic map (n) port map (a, sel(1 downto 0), cin, f2, cout2(0));
+        alu_d_intance: alu_d generic map (n) port map (a, cin, sel(1 downto 0), f3, cout3(0));
         mux4_f: mux4 generic map (n) port map (f0, f1, f2, f3, sel(3 downto 2), f);
-        mux4_cout: mux4 generic map (1) port map (cout0, cout1, cout2, cout3, sel(3 downto 2), cout);
+        mux4_cout: mux4 generic map (1) port map (
+            cout0, 
+            cout1, 
+            cout2, 
+            cout3, 
+            sel(3 downto 2), 
+            cout4
+        );
 
-        
+        cout <= cout4(0);
 end architecture alu_arch;
 
